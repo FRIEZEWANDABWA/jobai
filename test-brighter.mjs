78ff5a2465300c9f67349b2231211ce10107323e
friezewandabwa <friezekw@gmail.com>
2026-05-23 21:12:04 +0300

    chore: remove diagnostic scripts and html snapshots from repo, add to gitignore

--- test-brighter.mjs
+++
@@ -1,7 +0,0 @@
-import * as cheerio from 'cheerio';
-import fs from 'fs';
-const html = fs.readFileSync('brighter.html', 'utf8');
-const $ = cheerio.load(html);
-$('a[href*="/listings/"]').each((i, el) => {
-    console.log($(el).attr('href'), $(el).text().trim().replace(/\s+/g, ' '));
-});
